d8568c279242332be4647e68275d17dd5b40c4f9
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-11 14:30:20 +0200

    update styles and add GitHub logo

--- src/entities/user/ui/UserCard.tsx
+++ src/entities/user/ui/UserCard.tsx
@@ -7,15 +7,22 @@ type Props = {
 export const UserCard = ({ user }: Props) => {
   return (
     <div className="user-card">
-      <img src={user.avatar_url} alt={`${user.login} avatar`} width={120} />
-
-      <h2>{user.login}</h2>
-
-      <p>Repositories: {user.public_repos}</p>
-
-      <a href={user.html_url} target="_blank" rel="noreferrer">
-        GitHub Profile
-      </a>
+      <img
+        src={user.avatar_url}
+        alt={`${user.login} avatar`}
+        width={120}
+      />
+      <div className="user-card__info">
+        <h2>{user.login}</h2>
+        <p>Repositories: {user.public_repos}</p>
+        <a
+          href={user.html_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub Profile
+        </a>
+      </div>
     </div>
-  );
-};
+  )
+}
